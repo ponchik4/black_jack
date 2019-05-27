da2ba75fd8565e66034264eb5691bea899f639d8
rysa <rysinda4@gmail.com>
2019-05-27 22:04:48 +0300

    11

--- hand.rb
+++ hand.rb
@@ -6,15 +6,15 @@ class Hand
     @cards = []
   end
 
-  def ace_exist
-    @cards.select { |card| card.rank == 'A' }
+  def ace_exist?
+    @cards.select { |card| card.rank == 'A' }.any?
   end
 
   def card_sum
     sum = 0
     @cards.each do |card|
       sum += card.value
-      sum -= 10 if ace_exist && sum > 21
+      sum -= 10 if ace_exist? && sum > 21
     end
     sum
   end
